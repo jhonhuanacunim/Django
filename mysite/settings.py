ac8f91f3070641b3a29e4b53db93d2a32a769481
jhonhuanacunim <jhonhuanacunim@unjbg.edu.pe>
2024-01-07 13:46:08 -0500

    activando la muestra de errores

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret! 'django-insecure-p)wxsav&3_@iuzysg68wki_y5*d6iqi0du3xizllzrrbeinhyo'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+# SECURITY WARNING: don't run with debug turned on in production! 'RENDER' not in os.environ
+DEBUG = True
 ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
